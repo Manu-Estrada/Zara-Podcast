26b5328709b3da71b309edea38569452537f0b5c
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-03-24 10:06:25 +0100

    first commit

---
+++ frontend/src/components/Filter.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-text-field v-model="inputValue" label="Filter podcast ..." />
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        inputValue: "",
+      };
+    },
+  };
+  </script>
