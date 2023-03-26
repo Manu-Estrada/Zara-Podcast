c949121c1e37b4360c64668e5bca510569e95c3a
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-03-26 19:07:56 +0200

    styles card changes

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 <template>
     <v-card
       class="mx-auto"
-      max-width="400"
+      max-width="250"
     >
       <v-img
         class="align-end text-white"
@@ -21,9 +21,9 @@ const props = defineProps({
       <v-card-subtitle class="pt-4">
         {{podcast['title'].label  }}
       </v-card-subtitle>
-      <v-card-text>
-        <div>Whitehaven Beach</div>
-        <div>Whitsunday Island, Whitsunday Islands</div>
-      </v-card-text>
+      <!-- <v-card-text>
+        <div></div>
+        <div></div>
+      </v-card-text> -->
     </v-card>
   </template>
